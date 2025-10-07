871204f75359b7130d4d84fc2ca7f8e5c569d6ea
SilverAkatoki <3125295783@qq.com>
2025-10-08 00:51:33 +0800

    更改错误的网站标题 + 解决刷新404的问题

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 
 import Article from "@/views/Article/index.vue";
 import Articles from "@/views/Articles.vue";
@@ -29,7 +29,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes
 });
 
