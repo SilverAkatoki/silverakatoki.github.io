be5323e73506744c297712f086a961ae6c81588e
SilverAkatoki <3125295783@qq.com>
2025-12-22 22:13:14 +0800

    Updates

--- src/components/common/Readme.vue
+++
@@ -1,7 +0,0 @@
-<!-- 单独提取出来，手动编写 -->
-<template>
-  <div>
-    没有写完 Readme，所以留个空出来 <br />
-    真的没写完
-  </div>
-</template>
--- src/types/home.ts
+++
@@ -1,9 +0,0 @@
-export interface FriendLink {
-  name: string;
-  url: string;
-};
-
-export interface SiteInfo {
-  titleSentences: string[];
-  friendLink: FriendLink[];
-};
